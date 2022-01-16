4e17b2e4718d701f4274983180ef57ac16da6988
Waleed <waleedthedev@gmail.com>
2022-01-16 19:41:50 +0500

    Clean up and setup according to style guides

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,139 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  // import Footer from "./lib/footer.svelte"
+  
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Spartan:wght@500;700&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <!-- Item Start -->
+  Photosnap New! Featured Senior Frontend Developer 1d ago Full Time USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Senior
+  <!-- Languages -->
+  HTML CSS JavaScript
+  <!-- Item End -->
 
-  <Counter />
+  <!-- Item Start -->
+  Manage New! Featured Fullstack Developer 1d ago Part Time Remote
+  <!-- Role -->
+  Fullstack
+  <!-- Level -->
+  Midweight
+  <!-- Languages -->
+  Python
+  <!-- Tools -->
+  React
+  <!-- Item End -->
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
+  <!-- Item Start -->
+  Account New! Junior Frontend Developer 2d ago Part Time USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  React Sass
+  <!-- Item End -->
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <!-- Item Start -->
+  MyHome Junior Frontend Developer 5d ago Contract USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  CSS JavaScript
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Loop Studios Software Engineer 1w ago Full Time Worldwide
+  <!-- Role -->
+  Fullstack
+  <!-- Level -->
+  Midweight
+  <!-- Languages -->
+  JavaScript Ruby
+  <!-- Tools -->
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  FaceIt Junior Backend Developer 2w ago Full Time UK only
+  <!-- Role -->
+  Backend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  Ruby
+  <!-- Tools -->
+  RoR
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Shortly Junior Developer 2w ago Full Time Worldwide
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  HTML JavaScript
+  <!-- Tools -->
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Insure Junior Frontend Developer 2w ago Full Time USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  Vue Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Eyecam Co. Full Stack Engineer 3w ago Full Time Worldwide
+  <!-- Role -->
+  Fullstack
+  <!-- Level -->
+  Midweight
+  <!-- Languages -->
+  JavaScript Python
+  <!-- Tools -->
+  Django
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  The Air Filter Company Front-end Dev 1mo ago Part Time Worldwide
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  React Sass
+  <!-- Item End -->
+
+  <!-- <Footer /> -->
 </main>
 
 <style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,28 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            'desaturated-dark-cyan': 'hsl(180, 29%, 50%)',
+          },
+          neutral: {
+            'light-grayish-cyan-bg': 'hsl(180, 52%, 96%)',
+            'light-grayish-cyan-fltr': 'hsl(180, 31%, 95%)',
+            'dark-grayish-cyan': 'hsl(180, 8%, 52%)',
+            'very-dark-grayish-cyan': 'hsl(180, 14%, 20%)',
+          },
+        },
+      },
+      fontFamily: {
+        spartan: ['Spartan', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
